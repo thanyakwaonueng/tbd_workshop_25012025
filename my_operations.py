4e457b5827615c7d2733e9219c5fde93b4b4d0ca
cheek <gamsasivimon@gmail.com>
2025-01-25 14:01:16 +0700

    change2

--- my_operations.py
+++ my_operations.py
@@ -1,14 +1,11 @@
 # Member A: edit this function.
 def add(num1: int, num2: int) -> None:
-    print("WOOOOOOOOOOOOOOOOOOOOOOOOO");
-    # FUCK YOUU
-    print(num1, "+", num2, "is", res)
-
+    pass
 
 # Member B: edit this function.
 def diff(num1: int, num2: int) -> None:
     # Edit code under this line
-    res = num1-num2
+    # res = num1-num2
     print(num1, "-", num2, "is", res)
 
 
