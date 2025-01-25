94d9da363b2fbfc352e21ee7f85d808e300a6cb9
thanyakwaonueng <kwaonueng@gmail.com>
2025-01-25 14:11:55 +0700

    boring ahh commit

--- my_operations.py
+++ my_operations.py
@@ -1,7 +1,6 @@
 # Member A: edit this function.
 def add(num1: int, num2: int) -> None:
-    #OH NOOOOOOOOOOOOOOOOOOOOO
-    print("oh my godd", num1 + num2);
+    print("FUCK YOUUUUUUUUUUUUUUUUUUUUUUU");
 
 # Member B: edit this function.
 def diff(num1: int, num2: int) -> None:
