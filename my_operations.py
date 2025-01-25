77990f60dac5c0f50ebcaaea9a1e297f5499ecef
cheek <gamsasivimon@gmail.com>
2025-01-25 14:12:26 +0700

    change55

--- my_operations.py
+++ my_operations.py
@@ -8,6 +8,7 @@ def diff(num1: int, num2: int) -> None:
     res = num1-num2
     print(num1, "-", num2, "is", res)
     print(".......................aheeyeeeeee")
+    print(".......................ohhhhhhh my godddddddddd")
 
 
 # Member C: edit this function.
@@ -15,4 +16,5 @@ def mul(num1: int, num2: int) -> None:
     # Edit code under this line
     res = num1*num2
     print(num1, "*", num2, "is", res)
+    print(".......................ohhhhhhh my godddddddddd")
     print(".......................ohhhhhhh my godddddddddd")
