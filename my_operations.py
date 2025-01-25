7309d23db3b9eed228ae0db7226d0919817ccfd3
thanyakwaonueng <kwaonueng@gmail.com>
2025-01-25 14:23:01 +0700

    uuu

--- my_operations.py
+++ my_operations.py
@@ -1,20 +1,12 @@
 # Member A: edit this function.
 def add(num1: int, num2: int) -> None:
-    print("Lebron James")
+    print(num1 + num2);
 
 # Member B: edit this function.
 def diff(num1: int, num2: int) -> None:
-    # Edit code under this line
-    res = num1-num2
-    print(num1, "-", num2, "is", res)
-    print(".......................aheeyeeeeee")
-    print(".......................ohhhhhhh my godddddddddd")
+    print(num1 - num2);
 
 
 # Member C: edit this function.
 def mul(num1: int, num2: int) -> None:
-    # Edit code under this line
-    res = num1*num2
-    print(num1, "*", num2, "is", res)
-    print(".......................ohhhhhhh my godddddddddd")
-    print(".......................ohhhhhhh my godddddddddd")
+    print(num1 * num2);
